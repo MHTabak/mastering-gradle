892718aa685927f450de63d2adce1e72fe6e1449
MHTabak <michael_tabak@yahoo.com>
2017-09-07 17:17:32 -0700

    Simple Java file for CheckStyle and PMD to work on

---
+++ Chapter7/PluginProject/src/main/java/MainFile.java
@@ -0,0 +1,11 @@
+public class MainFile {
+
+// Simple class for checkstyle and pmd to work on
+
+    public static void main(String[] args) {
+       System.out.println("This is the main task");
+       Person person = new Person();
+       person.saySomething();
+    }
+}
+       
